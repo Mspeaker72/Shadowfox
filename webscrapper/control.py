98f42f3d2ed6140e3611a2be4c41b618bb8cf342
LungeloHlatshwayo <ianhlatshwayo@gmail.com>
2024-04-08 13:43:22 +0200

    proper navigation

---
+++ webscrapper/control.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://shadowfox.in/"
+
+
+# url = input("please provide url for site")
+
+def retrieve_contents(address: str):
+    response = requests.get(address)
+    html_content = response.content
+    return html_content
+
+
+soup = BeautifulSoup(retrieve_contents(url), 'html.parser')
+
+
+def find_all_links(context):
+    result = context.find_all("a")
+    # you can use get on class to find attribute
+    return [res.get("href") for res in result]
+
+
+def images(context):
+    result = context.find_all("img")
+    return [res.get("src") for res in result]
+
+
+print(find_all_links(soup))
+print(images(soup))
+
